c6f0691c72b0723a98cd573ee697fb7080c57b61
Chris G. Clapp <chris@chisosdesigns.com>
2014-01-21 15:06:26 -0700

    Included basic loop in index.php so that something will show up

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 ?>
 		<!--  Begin index.php -->
 		
+			<?php
+			
+				// !Basic Loop here
+				if(have_posts()) {
+					while(have_posts()) {
+						the_post();
+						echo "<h1>" . get_the_title() . "</h1>\n";
+						the_content();
+					}
+				}
+			
+			?>
 		
 		<!--  End index.php -->
 <?php
